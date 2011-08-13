1fcea3fc9d1ad93c6993fb2b26b57f4ca1d0d7d3
McNeil <mcneil@gmx.net>
2011-08-13 18:04:07 +0200

    project init

---
+++ Pong/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace Pong
+{
+    partial class Window
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PlayArea = new System.Windows.Forms.Panel();
+            this.Blue = new System.Windows.Forms.Panel();
+            this.PlayArea.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // PlayArea
+            // 
+            this.PlayArea.Controls.Add(this.Blue);
+            this.PlayArea.Location = new System.Drawing.Point(10, 10);
+            this.PlayArea.Name = "PlayArea";
+            this.PlayArea.Size = new System.Drawing.Size(610, 420);
+            this.PlayArea.TabIndex = 0;
+            // 
+            // Blue
+            // 
+            this.Blue.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.Blue.BackColor = System.Drawing.Color.Blue;
+            this.Blue.Location = new System.Drawing.Point(595, 345);
+            this.Blue.Margin = new System.Windows.Forms.Padding(0);
+            this.Blue.Name = "Blue";
+            this.Blue.Size = new System.Drawing.Size(15, 75);
+            this.Blue.TabIndex = 0;
+            // 
+            // Window
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(632, 449);
+            this.Controls.Add(this.PlayArea);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Window";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "GUID-Pong";
+            this.PlayArea.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel PlayArea;
+        private System.Windows.Forms.Panel Blue;
+    }
+}
+
